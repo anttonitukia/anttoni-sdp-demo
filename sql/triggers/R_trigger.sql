4974a9d99c0acb429d32d5232f81576f13713ae4
Anttoni Tukia <anttoni.tukia@solita.fi>
2026-05-04 12:34:36 +0300

    add ssh tuneling and flyway to pipeline

---
+++ sql/triggers/R_trigger.sql
@@ -0,0 +1,4 @@
+CREATE TRIGGER trigger_insert_last_name
+AFTER INSERT ON "dm-datasource-schema-4".gd_customer
+FOR EACH ROW
+EXECUTE FUNCTION "dm-datasource-schema-4".insert_last_name();
--- sql/triggers/trigger.sql
+++
@@ -1,4 +0,0 @@
-CREATE TRIGGER "dm-datasource-schema-4"."trigger_insert_last_name"
-AFTER INSERT ON "dm-datasource-schema-4"."gd_customer"
-FOR EACH ROW
-EXECUTE FUNCTION "dm-datasource-schema-4".insert_last_name();
